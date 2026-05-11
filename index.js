0e01f5283598104fe7111bb2b5b030ca52fd861e
JoBe <165585785+JoBeGaming@users.noreply.github.com>
2026-05-11 08:36:06 +0200

    Comment everything in `index.js` so we don't get any possible issues for now.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // TODO: Make this work for all subs (pages), and add a language changer
 
-function setSelfToLanguage(lang) {
-  if (!location.pathname.startswith(`/${lang}`)) {
-    // Won't work
-    location.pathname = `https://focuscharms.github.io/website/translations/${lang}/${location.pathname}.html`
-  }
-}
+// function setSelfToLanguage(lang) {
+//   if (!location.pathname.startswith(`/${lang}`)) {
+//     // Won't work
+//     location.pathname = `https://focuscharms.github.io/website/translations/${lang}/${location.pathname}.html`
+//   }
+// }
 
-const lang = "en"; //getLanguage();
+// const lang = "en"; //getLanguage();
 
-if (!location.pathname.startsWith(`/${lang}/`)) {
-  location.pathname = `translation/${lang}${location.pathname}`;
-}
+// if (!location.pathname.startsWith(`/${lang}/`)) {
+//   location.pathname = `translation/${lang}${location.pathname}`;
+// }
